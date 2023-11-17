750959a77e937273c8bb729ed2e09232cb14fb39
Andy Wu <andyww09@gmail.com>
2023-11-17 12:10:48 -0800

    add functional component change

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ db.sequelize.sync()
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to my application." });
 });
 
 require("./app/routes/tutorial.routes")(app);
