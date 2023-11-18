c9184a80e35f818dbeedf092fb1d401500182d39
Andy Wu <andyww09@gmail.com>
2023-11-18 15:51:11 -0800

    create frontend-js2 migration

---
+++ frontend-js2/src/components/add-tutorial.component.js
@@ -0,0 +1,188 @@
+import React, { Component } from "react";
+import TutorialDataService from "../services/tutorial.service";
+import { useState } from "react";
+
+export default function AddTutorial(){
+  const [submitted, setSubmitted] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  function saveTutorial(){
+    var data = {
+      title: title,
+      description: description
+    };
+
+    TutorialDataService.create(data)
+      .then(response => {
+        setSubmitted(true);
+        // this.setState({
+        //   id: response.data.id,
+        //   title: response.data.title,
+        //   description: response.data.description,
+        //   published: response.data.published,
+        //   submitted: true
+        // });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+  function newTutorial(){
+    setSubmitted(false);
+    setTitle("");
+    setDescription("");
+  }
+  return (
+      <div className="submit-form">
+      {submitted ? (
+        <div>
+          <h4>You submitted successfully!</h4>
+          <button className="btn btn-success" onClick={newTutorial}>
+            Add
+          </button>
+        </div>
+      ) : (
+        <div>
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              required
+              value={title}
+              onChange={e=>setTitle(e.target.value)}
+              name="title"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="description">Description</label>
+            <input
+              type="text"
+              className="form-control"
+              id="description"
+              required
+              value={description}
+              onChange={e=>setDescription(e.target.value)}
+              name="description"
+            />
+          </div>
+
+          <button onClick={saveTutorial} className="btn btn-success">
+            Submit
+          </button>
+        </div>
+      )}
+    </div>
+  )
+}
+// export default class AddTutorial extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.onChangeTitle = this.onChangeTitle.bind(this);
+//     this.onChangeDescription = this.onChangeDescription.bind(this);
+//     this.saveTutorial = this.saveTutorial.bind(this);
+//     this.newTutorial = this.newTutorial.bind(this);
+
+//     this.state = {
+//       id: null,
+//       title: "",
+//       description: "", 
+//       published: false,
+//       submitted: false
+//     };
+//   }
+
+//   onChangeTitle(e) {
+//     this.setState({
+//       title: e.target.value
+//     });
+//   }
+
+//   onChangeDescription(e) {
+//     this.setState({
+//       description: e.target.value
+//     });
+//   }
+
+//   saveTutorial() {
+//     var data = {
+//       title: this.state.title,
+//       description: this.state.description
+//     };
+
+//     TutorialDataService.create(data)
+//       .then(response => {
+//         this.setState({
+//           id: response.data.id,
+//           title: response.data.title,
+//           description: response.data.description,
+//           published: response.data.published,
+//           submitted: true
+//         });
+//         console.log(response.data);
+//       })
+//       .catch(e => {
+//         console.log(e);
+//       });
+//   }
+
+//   newTutorial() {
+//     this.setState({
+//       id: null,
+//       title: "",
+//       description: "",
+//       published: false,
+//       submitted: false
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div className="submit-form">
+//         {this.state.submitted ? (
+//           <div>
+//             <h4>You submitted successfully!</h4>
+//             <button className="btn btn-success" onClick={this.newTutorial}>
+//               Add
+//             </button>
+//           </div>
+//         ) : (
+//           <div>
+//             <div className="form-group">
+//               <label htmlFor="title">Title</label>
+//               <input
+//                 type="text"
+//                 className="form-control"
+//                 id="title"
+//                 required
+//                 value={this.state.title}
+//                 onChange={this.onChangeTitle}
+//                 name="title"
+//               />
+//             </div>
+
+//             <div className="form-group">
+//               <label htmlFor="description">Description</label>
+//               <input
+//                 type="text"
+//                 className="form-control"
+//                 id="description"
+//                 required
+//                 value={this.state.description}
+//                 onChange={this.onChangeDescription}
+//                 name="description"
+//               />
+//             </div>
+
+//             <button onClick={this.saveTutorial} className="btn btn-success">
+//               Submit
+//             </button>
+//           </div>
+//         )}
+//       </div>
+//     );
+//   }
+// }
